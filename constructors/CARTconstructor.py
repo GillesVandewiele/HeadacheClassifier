22c27dff023be4bd18e4c07fe8b39c5982510c8b
kianilannoye <kiani_lannoye@hotmail.com>
2016-03-07 12:10:30 +0100

    kleurtjes printen

--- constructors/CARTconstructor.py
+++ constructors/CARTconstructor.py
@@ -1,13 +1,11 @@
 import subprocess
 
 import numpy as np
-from skimage.measure.tests.test_fit import test_ransac_invalid_input
-from sklearn.externals.six import StringIO
-from sklearn import tree
 from pandas import DataFrame, read_csv, Series
 from sklearn.cross_validation import KFold
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
+from colors import bcolors
 from constructors.treeconstructor import TreeConstructor
 from decisiontree import DecisionTree
 
@@ -25,7 +23,7 @@ class CARTconstructor(TreeConstructor):
 
     def construct_tree(self, training_feature_vectors, labels):
         self.features = list(training_feature_vectors.columns[:4])
-        print"* features:", self.features
+        # print"* features:", self.features
 
         self.y = labels['cat']
         self.X = training_feature_vectors[self.features]
@@ -52,7 +50,7 @@ class CARTconstructor(TreeConstructor):
         labels.sort()
         labels = map(str, labels)
         # labels = labelnames.unique()
-        print labels
+        # print labels
         with open(filename + ".dot", 'w') as f:
             export_graphviz(tree.dt, out_file=f,
                             feature_names=feature_names, class_names=labels)
@@ -63,6 +61,107 @@ class CARTconstructor(TreeConstructor):
         except:
             exit("Could not run dot, ie graphviz, to "
                  "produce visualization")
+
+    def printTree(self,):
+        # Using those arrays, we can parse the tree structure:
+
+        n_nodes = self.dt.tree_.node_count
+        children_left = self.dt.tree_.children_left
+        children_right = self.dt.tree_.children_right
+        feature = self.dt.tree_.feature
+        threshold = self.dt.tree_.threshold
+
+
+        # The tree structure can be traversed to compute various properties such
+        # as the depth of each node and whether or not it is a leaf.
+        node_depth = np.zeros(shape=n_nodes)
+        is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+        stack = [(0, -1)]  # seed is the root node id and its parent depth
+        while len(stack) > 0:
+            node_id, parent_depth = stack.pop()
+            node_depth[node_id] = parent_depth + 1
+
+            # If we have a test node
+            if children_left[node_id] != children_right[node_id]:
+                stack.append((children_left[node_id], parent_depth + 1))
+                stack.append((children_right[node_id], parent_depth + 1))
+            else:
+                is_leaves[node_id] = True
+
+        print("The binary tree structure has %s nodes and has "
+              "the following tree structure:"
+              % n_nodes)
+        for i in range(n_nodes):
+            if is_leaves[i]:
+                print(bcolors.OKBLUE + "%snode=%s leaf node."  % (node_depth[i] * "\t", i)) + bcolors.ENDC
+            else:
+                print("%snode=%s test node: go to node %s if %s %s <= %s %s else to "
+                      "node %s."
+                      % (node_depth[i] * "\t",
+                         i,
+                         children_left[i],
+                          bcolors.BOLD,
+                         self.features[feature[i]],
+                         threshold[i],
+                         bcolors.ENDC,
+                         children_right[i],
+                         ))
+        print()
+
+
+    def convertToTree(self):
+        #       # Using those arrays, we can parse the tree structure:
+
+        ownDecisionTree = DecisionTree()
+        # label = naam feature waarop je splitst
+        # value = is de value van de feature waarop je splitst
+        # ownDecisionTree.
+
+
+        n_nodes = self.dt.tree_.node_count
+        children_left = self.dt.tree_.children_left
+        children_right = self.dt.tree_.children_right
+        feature = self.dt.tree_.feature
+        threshold = self.dt.tree_.threshold
+
+
+        # The tree structure can be traversed to compute various properties such
+        # as the depth of each node and whether or not it is a leaf.
+        node_depth = np.zeros(shape=n_nodes)
+        is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+        stack = [(0, -1)]  # seed is the root node id and its parent depth
+        while len(stack) > 0:
+            node_id, parent_depth = stack.pop()
+            node_depth[node_id] = parent_depth + 1
+
+            # If we have a test node
+            if children_left[node_id] != children_right[node_id]:
+                stack.append((children_left[node_id], parent_depth + 1))
+                stack.append((children_right[node_id], parent_depth + 1))
+            else:
+                is_leaves[node_id] = True
+
+        print("The binary tree structure has %s nodes and has "
+              "the following tree structure:"
+              % n_nodes)
+        for i in range(n_nodes):
+            
+            if is_leaves[i]:
+                print(bcolors.OKBLUE + "%snode=%s leaf node."  % (node_depth[i] * "\t", i)) + bcolors.ENDC
+            else:
+                print("%snode=%s test node: go to node %s if %s %s <= %s %s else to "
+                      "node %s."
+                      % (node_depth[i] * "\t",
+                         i,
+                         children_left[i],
+                          bcolors.BOLD,
+                         self.features[feature[i]],
+                         threshold[i],
+                         bcolors.ENDC,
+                         children_right[i],
+                         ))
+        print()
+
 
 
 # outlook = np.asarray([0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2]*1)
@@ -111,8 +210,10 @@ for train, test in kf:
 
     tree_constructor.construct_tree(train_feature_vectors_df.copy(), train_labels_df)
     tree_constructor.visualize_tree(tree_constructor.features, train_labels_df[['cat']], "tree" + str(i))
-
+    # tree_constructor.printTree()
+    tree_constructor.convertToTree()
     i += 1
+    print "\n\n-------------------------------\n\n"
     # tree_constructor.set_error_rate(decision_tree, test_feature_vectors_df.copy(), test_labels_df.copy())
     #
 
