bf5599586a6162f9974cc29aad2c3c9c1ba01b6e
kianilannoye <kiani_lannoye@hotmail.com>
2016-03-26 12:29:07 +0100

    Updates RF

--- benchmarks/neuralnet.py
+++ benchmarks/neuralnet.py
@@ -28,7 +28,7 @@ def build_nn(nr_features):
         #                              input_var=input_var)
 
         # input_shape=(None, 1, 28, 28),
-        input_shape=(None,nr_features),
+        input_shape=(None, nr_features),
         hidden_num_units=80,
         # hidden2_num_units=500,
         # hidden3_num_units=200,
@@ -47,7 +47,7 @@ def build_nn(nr_features):
         update_momentum=0.9,
 
         max_epochs=8000,
-        verbose=0,
+        verbose=0, #set this to 1, if you want to check the val and train scores for each epoch while training.
     )
     return net1
 
@@ -84,12 +84,12 @@ def local_test(feature_vectors_df, labels_df, k=2):
         # Fit our model
 
         y_train = np.reshape(np.asarray(y_train, dtype='int32'), (-1, 1)).ravel()
-        print y_train
-        print "Train feature vectors shape: " + X_train.shape.__str__()
-        print "Train labels shape:" + len(y_train).__str__()
-
-        print "X_train as array shape: " + str(X_train.shape)
-        print "y_train as array shape: " + str(np.reshape(np.asarray(y_train), (-1, 1)).shape)
+        # print y_train
+        # print "Train feature vectors shape: " + X_train.shape.__str__()
+        # print "Train labels shape:" + len(y_train).__str__()
+        #
+        # print "X_train as array shape: " + str(X_train.shape)
+        # print "y_train as array shape: " + str(np.reshape(np.asarray(y_train), (-1, 1)).shape)
 
         model.fit(X_train, np.reshape(np.asarray(y_train), (-1, 1)).ravel())
 
@@ -100,75 +100,25 @@ def local_test(feature_vectors_df, labels_df, k=2):
 
         model = None
         del model
-        # cm = confusion_matrix(y_test, preds)
-        # plt.matshow(cm)
-        # plt.title('Confusion matrix')
-        # plt.colorbar()
-        # plt.ylabel('True label')
-        # plt.xlabel('Predicted label')
-        # plt.show()
+        cm = confusion_matrix(y_test, preds)
+        plt.matshow(cm)
+        plt.title('Confusion matrix')
+        plt.colorbar()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
 
-        print preds.tolist()
-        print "number of ones: " + str(sum(preds))
-        print y_test
-        print c
-        print ((len(c) * 1.0) / (len(y_test) * 1.0))
+        # print preds.tolist()
+        # print "number of ones: " + str(sum(preds))
+        # print y_test
+        # print c
+        print "Accuracy for fold: " + str(((len(c) * 1.0) / (len(y_test) * 1.0))) + "\n\n\n\n\n-----------------------------\n\n\n"
 
-def load_dataset():
-    # We first define a download function, supporting both Python 2 and 3.
-    if sys.version_info[0] == 2:
-        from urllib import urlretrieve
-    else:
-        from urllib.request import urlretrieve
 
-    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
-        print("Downloading %s" % filename)
-        urlretrieve(source + filename, filename)
-
-    # We then define functions for loading MNIST images and labels.
-    # For convenience, they also download the requested files if needed.
-    import gzip
-
-    def load_mnist_images(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the inputs in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them to monochrome 2D images,
-        # following the shape convention: (examples, channels, rows, columns)
-        data = data.reshape(-1, 1, 28, 28)
-        # The inputs come as bytes, we convert them to float32 in range [0,1].
-        # (Actually to range [0, 255/256], for compatibility to the version
-        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
-        return data / np.float32(256)
-
-    def load_mnist_labels(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the labels in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
-        return data
-
-    # We can now download and read the training and test set images and labels.
-    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
-    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
-    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
-    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
-
-    # We reserve the last 10000 training examples for validation.
-    X_train, X_val = X_train[:-10000], X_train[-10000:]
-    y_train, y_val = y_train[:-10000], y_train[-10000:]
-
-    # We just return all the arrays in order, as expected in main().
-    # (It doesn't matter how we do this as long as we can read them again.)
-    return X_train, y_train, X_val, y_val, X_test, y_test
 columns = ['age', 'sex', 'chest pain type', 'resting blood pressure', 'serum cholestoral', 'fasting blood sugar', \
            'resting electrocardio', 'max heartrate', 'exercise induced angina', 'oldpeak', 'slope peak', \
            'number of vessels', 'thal', 'disease']
-df = read_csv('heart.dat', sep=' ')
+df = read_csv('../data/heart.dat', sep=' ')
 # df = df.iloc[np.random.permutation(len(df))]
 # df = df.reset_index(drop=True)
 df.columns = columns
@@ -189,31 +139,7 @@ labels_df = DataFrame()
 labels_df['cat'] = df['disease'].copy()
 features_df = df.copy()
 features_df = features_df.drop('disease', axis=1)
-features_df = (features_df-features_df.mean())/(features_df.max()-features_df.min())
+features_df = (features_df - features_df.mean()) / (features_df.max() - features_df.min())
 features_column_names = features_df.columns
 
 local_test(feature_vectors_df=features_df, labels_df=labels_df, k=10)
-# X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
-# model = build_nn(nr_features=X_train.shape[1])
-# model.initialize()
-# layer_info = PrintLayerInfo()
-# layer_info(model)
-#
-# # Fit our model
-#
-# y_train = np.reshape(np.asarray(y_train, dtype='int32'), (-1, 1)).ravel()
-# print y_train
-# print "Train feature vectors shape: " + X_train.shape.__str__()
-# print "Train labels shape:" + len(y_train).__str__()
-#
-# print "X_train as array shape: " + str(X_train.shape)
-# print "y_train as array shape: " + str(np.reshape(np.asarray(y_train), (-1, 1)).shape)
-#
-# model.fit(X_train, np.reshape(np.asarray(y_train), (-1, 1)).ravel())
-#
-# preds = model.predict(X_test)
-# c = []
-# [c.append(preds[i]) if preds[i] == y_train[i] else None for i in range(min(len(y_train), len(preds)))]
-# # checks = len([i for i, j in zip(preds, np.reshape(np.asarray(y_train), (-1, 1))) if i == j])
-# print preds
-# print ((len(c) * 1.0) / (len(y_test) * 1.0))
--- benchmarks/randomforest.py
+++ benchmarks/randomforest.py
@@ -1,14 +1,28 @@
 # Read csv into pandas frame
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 from pandas import read_csv, DataFrame
 from sklearn.ensemble import RandomForestClassifier
 
-# TODO: nieuwe methode naam please
-def evaluate_trees(features_df, labels_df, n_folds=2):
+
+def plot_confusion_matrix(cm, title='Confusion matrix'):
+    fig = plt.figure()
+    cm = np.divide(cm, len(cm))
+    cm = np.divide(cm, np.matrix.sum(np.asmatrix(cm))).round(3)
+
+    ax = fig.add_subplot(1, 1, 1)
+    cax = ax.matshow(cm, cmap=plt.get_cmap('RdYlGn'))
+    for (j, i), label in np.ndenumerate(cm):
+        ax.text(i, j, label, ha='center', va='center')
+    fig.colorbar(cax)
+    plt.show()
+
+
+def evaluate_random_forests(features_df, labels_df, n_folds=2):
     kf = sklearn.cross_validation.KFold(len(labels_df.index), n_folds=n_folds)
-    # TODO: kan je ook geen confusion matrices genereren?
-    tree_confusion_matrices = {}
+
+    confusion_matrices_folds = []
 
     for train, test in kf:
         X_train = DataFrame(features_df, index=train)
@@ -16,27 +30,8 @@ def evaluate_trees(features_df, labels_df, n_folds=2):
         y_train = DataFrame(labels_df, index=train)
         y_test = DataFrame(labels_df, index=test)
 
-        rf = RandomForestClassifier(n_estimators=1000, n_jobs=-1)
+        rf = RandomForestClassifier(n_estimators=500, n_jobs=-1)
         rf.fit(X_train.values.tolist(), y_train['cat'].tolist())
-        # importances = rf.feature_importances_
-        # std = np.std([tree.feature_importances_ for tree in rf.estimators_],
-        #      axis=0)
-        # indices = np.argsort(importances)[::-1]
-        #
-        # # Print the feature ranking
-        # print("Feature ranking:")
-        #
-        # for f in range(X_train.shape[1]):
-        #     print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-        #
-        # # Plot the feature importances of the forest
-        # plt.figure()
-        # plt.title("Feature importances")
-        # plt.bar(range(X_train.shape[1]), importances[indices],
-        #        color="r", yerr=std[indices], align="center")
-        # plt.xticks(range(X_train.shape[1]), indices)
-        # plt.xlim([-1, X_train.shape[1]])
-        # plt.show()
 
         predicted_labels = []
         for index, vector in enumerate(X_test.values):
@@ -49,11 +44,25 @@ def evaluate_trees(features_df, labels_df, n_folds=2):
         accuracy = equal_positions / len(predicted_labels)
         print accuracy
 
+        # Save the confusion matrix for this fold and plot it
+        confusion_matrix = sklearn.metrics.confusion_matrix(y_test_np, predicted_labels)
+        confusion_matrices_folds.append(confusion_matrix)
+        # plt.figure()
+        # cm_normalized = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
+
+    #     Let's plot the confusion matrix of the avarage confusion matrix
+    sum = confusion_matrices_folds[0] * 1.0
+    for i in range(1, len(confusion_matrices_folds)):
+        sum += confusion_matrices_folds[i]
+    sum /= len(confusion_matrices_folds)
+    plot_confusion_matrix(sum)
+
+
 # TODO: De run code moet hier weg
 columns = ['age', 'sex', 'chest pain type', 'resting blood pressure', 'serum cholestoral', 'fasting blood sugar', \
            'resting electrocardio', 'max heartrate', 'exercise induced angina', 'oldpeak', 'slope peak', \
            'number of vessels', 'thal', 'disease']
-df = read_csv('heart.dat', sep=' ')
+df = read_csv('../data/heart.dat', sep=' ')
 # df = df.iloc[np.random.permutation(len(df))]
 # df = df.reset_index(drop=True)
 df.columns = columns
@@ -76,26 +85,26 @@ features_df = df.copy()
 features_df = features_df.drop('disease', axis=1)
 features_column_names = features_df.columns
 
-rf = RandomForestClassifier(n_estimators=1000, n_jobs=-1)
+rf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
 rf.fit(DataFrame(features_df.values.tolist()), labels_df['cat'].tolist())
 importances = rf.feature_importances_
 std = np.std([tree.feature_importances_ for tree in rf.estimators_],
-     axis=0)
+             axis=0)
 indices = np.argsort(importances)[::-1]
 
 # Print the feature ranking
 print("Feature ranking:")
 
 for f in range(DataFrame(features_df.values.tolist()).shape[1]):
-    print("%d. feature %s [%d] (%f)" % (f + 1, features_column_names[f], indices[f], importances[indices[f]]))
+    print("%d. feature %s [%d] (%f)" % (f + 1, features_column_names[indices[f]], indices[f], importances[indices[f]]))
 
 # Plot the feature importances of the forest
 plt.figure()
 plt.title("Feature importances")
 plt.bar(range(DataFrame(features_df.values.tolist()).shape[1]), importances[indices],
-       color="r", yerr=std[indices], align="center")
+        color="r", yerr=std[indices], align="center")
 plt.xticks(range(DataFrame(features_df.values.tolist()).shape[1]), indices)
 plt.xlim([-1, DataFrame(features_df.values.tolist()).shape[1]])
 plt.show()
 
-evaluate_trees(features_df=features_df, labels_df=labels_df, n_folds=10)
+evaluate_random_forests(features_df=features_df, labels_df=labels_df, n_folds=10)
